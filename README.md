3c18c6e6b35d2605da6a02aa12a0015aeb0f98b2
Denis Aleksandrov <denal05@gmail.com>
2020-07-26 22:32:12 +0200

    Forked from https://github.com/andregardi/ts-express Returning the full book # 63 upon requesting http://localhost:3000/book/63/chapter/02/verses/3-4

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # kjv-bible-server
 Serving contents of the King James Version of the Bible (from Project Gutenberg) via Node.js
+
+## Boilerplate
+Forked from [https://github.com/andregardi/ts-express](https://github.com/andregardi/ts-express)
+
+Minimal boilerplate to start a Express.js project using TypeScript.
+
+For more information check out the article:
+
+[https://medium.com/@gardikoury/eb6bc73edd5d](https://medium.com/@gardikoury/eb6bc73edd5d)
+
+## Quick Start
+To install this dependency use:
+
+```
+npm install
+```
+To run ther server in development mode:
+```
+npm run dev
+```
+To run ther server in production mode:
+```
+npm run prod
+```
---
+++ app/app.ts
@@ -0,0 +1,42 @@
+// app/app.ts
+import express = require('express');
+import fs from 'fs';
+
+// Create a new express application instance
+const app: express.Application = express();
+
+
+app.get('/', function (req, res) {
+  fs.readFile('html/home.html', function (err, fileContents) {
+    if (err) {
+        return console.error(err);
+    }
+    //res.setHeader('Content-Type', 'application/json');
+    //res.send(JSON.stringify({ a: 1 }));
+    res.send(fileContents.toString());
+  });
+});
+
+app.get('/book/:bookId/chapter/:chapterId/verses/:fromV-:toV', function (req, res) {
+  // http://localhost:3000/book/01/chapter/02/verses/1-2
+  // {"bookId":"01","chapterId":"02","fromV":"1","toV":"2"}
+  /*
+  res.send(req.params)
+  res.send(req.params.bookId)
+  res.send(req.params.chapterId)
+  res.send(req.params.fromV)
+  res.send(req.params.toV)
+  */
+
+  fs.readFile('txt/pg80'+req.params.bookId+'.txt', function (err, fileContents) {
+    if (err) {
+        return console.error(err);
+    }
+    res.send(fileContents.toString());
+  });
+
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
